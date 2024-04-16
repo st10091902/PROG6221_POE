9b9ff9d486b19163a224c141296ddc6edbefd627
st10091902 <st10091902@vcconnect.edu.za>
2024-04-16 20:34:21 +0200

    Declared the arrays

--- Program.cs
+++ Program.cs
@@ -1,10 +1,35 @@
 ﻿namespace RecipeApplication
 {
-    internal class Program
+    
+    class Recipe
+    {
+        private string[] ingredients;
+        private double[] quantities;
+        private string[] units;
+        private string[] steps;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
